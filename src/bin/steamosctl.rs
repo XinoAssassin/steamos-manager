ee41fb45e5aeff0defd5081400fd07e7ba9567b6
Jeremy Whiting <jpwhiting@kde.org>
2025-06-13 16:57:08 -0600

    Draft: screenreader: Add TriggerAction functionality.
    Add functionality for various actions by sending left control key
    press and release events.
    Also add to steamosctl trigger_action support.
    NOTE: Stop Speaking action should be changed to use orca's new dbus
    api once that's packaged.

--- src/bin/steamosctl.rs
+++ src/bin/steamosctl.rs
@@ -10,6 +10,7 @@ use clap::{ArgAction, Parser, Subcommand};
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::io::Cursor;
+use std::time::{SystemTime, UNIX_EPOCH};
 use steamos_manager::cec::HdmiCecState;
 use steamos_manager::hardware::{FactoryResetKind, FanControlState};
 use steamos_manager::power::{CPUScalingGovernor, GPUPerformanceLevel, GPUPowerProfile};
@@ -20,7 +21,7 @@ use steamos_manager::proxy::{
     TdpLimit1Proxy, UpdateBios1Proxy, UpdateDock1Proxy, WifiDebug1Proxy, WifiDebugDump1Proxy,
     WifiPowerManagement1Proxy,
 };
-use steamos_manager::screenreader::ScreenReaderMode;
+use steamos_manager::screenreader::{ScreenReaderAction, ScreenReaderMode};
 use steamos_manager::wifi::{WifiBackend, WifiDebugMode, WifiPowerManagement};
 use zbus::fdo::{IntrospectableProxy, PropertiesProxy};
 use zbus::{zvariant, Connection};
@@ -251,6 +252,23 @@ enum Commands {
     SetScreenReaderMode {
         /// Valid modes are `browse`, `focus`
         mode: ScreenReaderMode,
+    },
+
+    /// Trigger screen reader action
+    TriggerScreenReaderAction {
+        /// Valid actions are:
+        ///  stop_talking
+        ///  read_next_word
+        ///  read_previous_word
+        ///  read_next_item
+        ///  read_previous_item
+        ///  move_to_next_landmark,
+        ///  move_to_previous_landmark,
+        ///  move_to_next_heading,
+        ///  move_to_previous_heading,
+        ///  toggle_mode,
+        ///
+        action: ScreenReaderAction,
     },
 }
 
@@ -612,6 +630,16 @@ async fn main() -> Result<()> {
         Commands::SetScreenReaderMode { mode } => {
             let proxy = ScreenReader0Proxy::new(&conn).await?;
             proxy.set_mode(*mode as u32).await?;
+        }
+        Commands::TriggerScreenReaderAction { action } => {
+            let proxy = ScreenReader0Proxy::new(&conn).await?;
+            let start = SystemTime::now();
+            let timestamp = start
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            proxy
+                .trigger_action(*action as u32, timestamp.as_secs())
+                .await?;
         }
     }
 
